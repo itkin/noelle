c5cfe11473d351b0ac3f64b0cc3a4e40380caae2
= <nicolas.papon@webflows.fr>
2010-11-16 22:42:35 +0100

    user controller show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < Admin::ApplicationController
   active_scaffold :user do |config|
-    config.list.columns = [:email]
+    config.show.columns = config.list.columns = [:email]
     config.update.columns = config.create.columns = [:email, :password, :password_confirmation] 
   end
 end
