7a2d3c3ce7615a0395c7f07989652b53a3ffc9da
= <nicolas.papon@webflows.fr>
2010-11-18 00:37:28 +0100

    fix pb sur tinymce

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -1,40 +1,48 @@
-function displayProject(timer){
-  setTimeout(function(){
-    window.location = $(".access_to_project").attr('href')
-  }, timer);
-}
+//Website application
 
-$(function(){
+(function($){
+  
+  Galleria.loadTheme('/javascripts/galleria/src/themes/noelle/galleria.noelle.js');
 
-  // Slideshow
-  $('#gallery').galleria({
-    width: 666,
-    height: 500,
-    counter: false
-  });
+  function displayProject(timer){
+    setTimeout(function(){
+      window.location = $(".access_to_project").attr('href')
+    }, timer);
+  }
+
+  $(function(){
+    
+    // Slideshow
+    $('#gallery').galleria({
+      width: 666,
+      height: 500,
+      counter: false
+    });
 
 
-  // Barre de défilement sur les pages custo
-  $("#page_content, #category_comment").jScrollPane();
+    // Barre de défilement sur les pages custo
+    $("#page_content, #category_comment").jScrollPane();
 
 
-  // Animations du menu
-  $('ul.categories > li > a').toggle(function(e){
-    if ($(this).attr('data-comment') == 'true'){
-      window.location = $(this).attr('href');
-    } else {
+    // Animations du menu
+    $('ul.categories > li > a').toggle(function(e){
+      if ($(this).attr('data-comment') == 'true'){
+        window.location = $(this).attr('href');
+      } else {
+        e.preventDefault();
+        var $ul = $(this).next('ul.projects');
+        if ($ul.length != 0)
+          $ul.slideToggle();
+      }
+    }, function(e){
       e.preventDefault();
       var $ul = $(this).next('ul.projects');
       if ($ul.length != 0)
         $ul.slideToggle();
-    }
-  }, function(e){
-    e.preventDefault();
-    var $ul = $(this).next('ul.projects');
-    if ($ul.length != 0)
-      $ul.slideToggle();
+    });
+
+
+
   });
-
-
-
-});
+  
+})(jQuery);
