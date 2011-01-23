c32ce563a2cc8747765d609441defaba25fcd80d
= <nicolas.papon@webflows.fr>
2011-01-23 17:11:13 +0100

    modifs camille

---
+++ db/migrate/20110123153958_add_attachment_document_picture_to_page.rb
@@ -0,0 +1,23 @@
+class AddAttachmentDocumentPictureToPage < ActiveRecord::Migration
+  def self.up
+    add_column :pages, :document_file_name, :string
+    add_column :pages, :document_content_type, :string
+    add_column :pages, :document_file_size, :integer
+    add_column :pages, :document_updated_at, :datetime
+    add_column :pages, :picture_file_name, :string
+    add_column :pages, :picture_content_type, :string
+    add_column :pages, :picture_file_size, :integer
+    add_column :pages, :picture_updated_at, :datetime
+  end
+
+  def self.down
+    remove_column :pages, :document_file_name
+    remove_column :pages, :document_content_type
+    remove_column :pages, :document_file_size
+    remove_column :pages, :document_updated_at
+    remove_column :pages, :picture_file_name
+    remove_column :pages, :picture_content_type
+    remove_column :pages, :picture_file_size
+    remove_column :pages, :picture_updated_at
+  end
+end
