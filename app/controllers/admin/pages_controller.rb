d45e53736c28c81deb1deb4d4c019ae491144110
= <nicolas.papon@webflows.fr>
2011-02-09 17:10:35 +0100

    modifs camille

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -2,10 +2,11 @@ class Admin::PagesController < Admin::ApplicationController
   active_scaffold :page do |config|
     
     config.actions.exclude :create
-    config.show.columns = config.update.columns = [:image, :picture, :document, :content]
+    config.show.columns = config.update.columns = [:image, :picture, :document, :document_2, :content]
     config.columns[:image].label = "Image du cadre"
     config.columns[:picture].label = "Image du texte"
     config.columns[:document].label = "Document joint"
+    config.columns[:document_2].label = "Document 2 joint"
     config.columns[:content].label = "Contenu"
   end
 end
