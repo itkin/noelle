cf0dfb50bf71172bb86ddd366e32b1f87e8229c4
Nicolas <nicolas.papon@webflows.fr>
2011-05-20 15:42:07 +0200

    changement nom du cokie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Noelle::Application.config.session_store :cookie_store, :key => '_noelle_session'
+Noelle::Application.config.session_store :cookie_store, :key => '_noelle_session_20062010'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
