8af14299e7db2f9eb7a8f9951d44257f1d5d9f14
Nicolas <nicolas.papon@webflows.fr>
2012-04-16 15:54:25 +0200

    deploy

---
+++ db/migrate/20120416134957_devises_columns.rb
@@ -0,0 +1,8 @@
+class DevisesColumns < ActiveRecord::Migration
+  def up
+    add_column :users, :reset_password_sent_at, :datetime
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111116181043) do
+ActiveRecord::Schema.define(:version => 20120416134957) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "position"
@@ -90,15 +90,16 @@ ActiveRecord::Schema.define(:version => 20111116181043) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "",    :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
-    t.string   "password_salt",                       :default => "",    :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                         :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "manage_newsletter",                   :default => false
+    t.boolean  "manage_newsletter",                     :default => false
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
